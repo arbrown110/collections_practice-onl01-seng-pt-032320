10cf0fb65de2fa3ee60b65f8d009b58093854ccc
chic-caller-7520 <arbrown110@gmail.com>
2020-04-04 14:06:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,80 @@
 
+def sort_array_asc (array)
+  array.sort 
+    
+end
+sort_array_asc([25, 7, 1])
+
+def sort_array_desc (array)
+    array.sort do |a,b|
+      b <=> a 
+    end  
+end
+
+sort_array_desc([25, 7, 14])
+
+def sort_char_count(array)
+  array.sort do | a, b|
+    a.length <=> b.length
+  end
+end
+
+sort_char_count(["dogs", "cat", "Horses"])
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+end      
+
+swap_elements(["blake", "ashley", "scott"])
+
+def reverse_array(array)
+  array.reverse
+end
+
+reverse_array(["blake", "ashley", "scott"])
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+kesha_maker(["blake", "ashley", "scott"])
+
+
+def find_a(array)
+  array.sort do |word|
+    word[0] == "a"
+  end
+end
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
+
+
+def sum_array(array)
+  sum = 0
+    array.each do |num|
+      sum+=num
+    end
+  sum
+end
+
+
+def sum_array(array)
+   array.inject do |sum,x|
+    sum + x
+   end
+end   
+
+sum_array([11,4,7,8,9,100,134]) 
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
+
+add_s(["hand","feet", "knee", "table"])
